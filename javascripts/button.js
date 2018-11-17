f6d66ad3ca88775ea178131d37790bfa470b013d
kostanzhoglo <jeffreywithers@gmail.com>
2018-11-17 07:00:49 -0500

    First commit. This is a playground, to test out basic functions, etc.

---
+++ javascripts/button.js
@@ -0,0 +1,17 @@
+// window.onload = function () {
+//
+  function dateTime() {
+    let d = new Date();
+    document.getElementById('result').innerHTML = "<h1>Today's date is " + d + "</h1>"
+  }
+
+
+// }
+
+
+
+
+// function test() {
+//   let words = "OR DOES I.  Does I?"
+//   document.getElementById('result').innerHTML = words
+// }
